b2bdef6245a069aaa31bea04dc87933b31b34795
Jan Toebes <jan@toebes.info>
2017-08-01 20:44:19 +0200

    -

--- 06_yesnotypeclass.scala
+++ 06_yesnotypeclass.scala
@@ -1,2 +1,51 @@
-:q!
+trait CanTruthy[A] { self =>
+  def truthys(a: A): Boolean
+}
 
+object CanTruthy {
+  def apply[A](implicit ev: CanTruthy[A]): CanTruthy[A] = ev
+  def truthys[A](f: A => Boolean): CanTruthy[A] = new CanTruthy[A] {
+    def truthys(a: A): Boolean = f(a)
+  }
+}
+
+trait CanTruthyOps[A] {
+  def self: A
+  implicit def F: CanTruthy[A]
+  final def truthy: Boolean = F.truthys(self)
+}
+
+object ToCanIsTruthyOps {
+  implicit def toCanIsTruthyOps[A](v: A)(implicit ev: CanTruthy[A]) =
+    new CanTruthyOps[A] {
+      def self = v
+      implicit def F: CanTruthy[A] = ev
+    }
+}
+
+import ToCanIsTruthyOps._
+
+implicit val intCanTruthy: CanTruthy[Int] = CanTruthy.truthys({
+  case 0 => false
+  case _ => true
+})
+
+implicit def listCanTruthy[A]: CanTruthy[List[A]] = CanTruthy.truthys({
+  case Nil => false
+  case _   => true
+})
+
+implicit val booleanCanTruthy: CanTruthy[Boolean] = CanTruthy.truthys(identity)
+
+10.truthy println
+
+List("foo").truthy println
+
+false.truthy println
+
+// If statement
+def truthyIf[A: CanTruthy, B, C](cond: A)(ifyes: => B)(ifno: => C) =
+  if (cond.truthy) ifyes
+  else ifno
+
+println (truthyIf (2 :: Nil) {"YEAH!"} {1})
